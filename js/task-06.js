23cdec78736154296f32ac89a005484ee3dad868
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-07-24 00:01:29 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,20 @@
 const inputTextRef = document.querySelector('input#validation-input');
 
 inputTextRef.addEventListener('blur', event => {
-  //   console.log('dataset :', inputTextRef.dataset.length);
-  //   console.log('value:', event.currentTarget.value.length);
+  // console.log('dataset :', inputTextRef.dataset.length);
+  // console.log('value:', event.currentTarget.value.length);
   if (Number(inputTextRef.dataset.length) === event.currentTarget.value.length) {
+    // console.log(inputTextRef.classList);
+
+    if (inputTextRef.classList.contains('invalid')) {
+      inputTextRef.classList.remove('invalid');
+    }
     inputTextRef.classList.add('valid');
   } else {
+    if (inputTextRef.classList.contains('valid')) {
+      inputTextRef.classList.remove('valid');
+    }
+
     inputTextRef.classList.add('invalid');
   }
 });
