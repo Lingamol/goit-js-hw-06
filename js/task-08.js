246df0e694bd1d01980d21988f4940eba95c7c38
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-07-23 23:26:15 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector('form.login-form');
 
+formRef.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Все поля должны быть заполнены!');
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnRef = document.querySelector('button.change-color');
+const textRef = document.querySelector('span.color');
+console.log(btnRef);
+btnRef.addEventListener('click', () => {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = backgroundColor;
+  textRef.textContent = backgroundColor;
+});
